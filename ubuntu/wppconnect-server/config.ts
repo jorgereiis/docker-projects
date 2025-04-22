6e7e7631f725d539870ebd852236b06486ea5abe
Jorge Reis <jreiisgalvao@gmail.com>
2025-04-22 01:14:13 -0300

    Up

--- ubuntu/wppconnect-server/config.ts
+++ ubuntu/wppconnect-server/config.ts
@@ -11,7 +11,7 @@ export default {
   maxListeners: 15,
   customUserDataDir: './userDataDir/',
   webhook: {
-    url: "http://nossopainel.com.br/webhook/",
+    url: null,
     autoDownload: true,
     uploadS3: false,
     readMessage: true,
